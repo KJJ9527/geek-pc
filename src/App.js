efa71dabcc19628bcedad099f5553c283ca02f7b
963707772@qq.com <963707772@qq.com>
2023-03-29 11:33:06 +0800

    配置别名

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import Layout from '@/pages/Layout'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import 'antd/dist/reset.css'
 import './App.scss'
 export default function App() {
-  return <div>极客园pc端</div>
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Layout} />
+          <Route path="/login" component={Login} />
+          {/* 404 */}
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
